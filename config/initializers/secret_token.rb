e21752db1d534723c3c79b1c05d89137f60e72cf
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-07-11 19:59:42 -0400

    shirtz errwhere

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shirtz::Application.config.secret_key_base = '10732fcc79a62c0787025a66f324d21e95f98f9fa943f979fade47899adf4192f5dd1d2acf9f7d0c130538153148e8c81e02395ac01ca3123d95f8299ddcfc9e'
