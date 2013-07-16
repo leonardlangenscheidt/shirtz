ef3f593eae83117745f5af49a50169b02c7dcb6b
Leonard Langenscheidt <leonardlangenscheidt@gmail.com>
2013-07-16 19:39:42 -0400

    added shirts listings with search functionality. added images

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Shirtz::Application.routes.draw do
+
+  resources:shirts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
